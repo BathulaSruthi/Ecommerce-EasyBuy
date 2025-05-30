fcb1cbf28a71536af8dc7af77f2c9a35f9df938b
BathulaSruthi <sruthibathula.2002@gmail.com>
2025-05-30 10:38:01 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,42 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> register</a>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> login</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact us</h3>
+         <a href="tel:1234567890"><i class="fas fa-phone"></i> +123 456 7899</a>
+         <a href="tel:22222233333"><i class="fas fa-phone"></i> +222 222 3333</a>
+         <a href="mailto:sruthibathula.2002@gmail.com"><i class="fas fa-envelope"></i> sruthibathula.2002@gmail.com</a>
+         <a href="https://maps.app.goo.gl/7qef5jEj2hNKsX9x6"><i class="fas fa-map-marker-alt"></i> Warangal, India - 506003 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+         <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+         <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+         
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Bathula Sruthi</span> | all rights reserved!</div>
+
+</footer>
